7a55a3e04acbe3a969fd8d12667a1fb8c4b44dec
Udara De Silva <udara28@live.com>
2020-10-20 22:17:12 -0400

    Adding hello_camera app

---
+++ vitis_app/hello_camera/build/CMakeFiles/hello_camera.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/app/src/decodethread.cpp" "/workspace/build/CMakeFiles/hello_camera.dir/app/src/decodethread.cpp.o"
+  "/workspace/app/src/dputhread.cpp" "/workspace/build/CMakeFiles/hello_camera.dir/app/src/dputhread.cpp.o"
+  "/workspace/app/src/guithread.cpp" "/workspace/build/CMakeFiles/hello_camera.dir/app/src/guithread.cpp.o"
+  "/workspace/app/src/mythread.cpp" "/workspace/build/CMakeFiles/hello_camera.dir/app/src/mythread.cpp.o"
+  "/workspace/app/src/sortthread.cpp" "/workspace/build/CMakeFiles/hello_camera.dir/app/src/sortthread.cpp.o"
+  "/workspace/app/test/usb_input_multi_threads_xcl_refinedet.cpp" "/workspace/build/CMakeFiles/hello_camera.dir/app/test/usb_input_multi_threads_xcl_refinedet.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_DRM"
+  "USE_KERNEL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../app/include"
+  "../HLS_V4l2s/include"
+  "/usr/include/drm"
+  "/usr/include/vitis/ai"
+  "/opt/xilinx/xrt/include"
+  "/usr/include/xrt"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspace/build/HLS_V4l2s/CMakeFiles/hls_v4l2s.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ vitis_app/hello_camera/build/HLS_V4l2s/CMakeFiles/test_hls_kernel.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/HLS_V4l2s/test/test_hls_kernel.cpp" "/workspace/build/HLS_V4l2s/CMakeFiles/test_hls_kernel.dir/test/test_hls_kernel.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../HLS_V4l2s/include"
+  "/opt/xilinx/xrt/include"
+  "/usr/include/xrt"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/workspace/build/HLS_V4l2s/CMakeFiles/hls_v4l2s.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
